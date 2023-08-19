612a075443db2bc851c17822600b68b11e61caf2
SanTacrZ <santacruzb97@gmail.com>
2023-08-19 11:15:21 -0500

    preguntando al usuario si quiere realizar un nuevo cálculo antes de salir

--- src/App.java
+++ src/App.java
@@ -5,17 +5,37 @@ public class App {
         Scanner entrada = new Scanner(System.in);
 
         double numero_1 = 0, numero_2 = 0;
-        System.out.println("Ingresa el primer numero ");
-        numero_1 = entrada.nextDouble();
-        System.out.println("Ingresa el segundo numero ");
-        numero_2 = entrada.nextDouble();
-        System.out.println("La suma es: " + (numero_1 + numero_2));
-        System.out.println("La resta es: " + (numero_1 - numero_2));
-        System.out.println("La multiplicacion es: " + (numero_1 * numero_2));
-        System.out.println("La division es: " + (numero_1 / numero_2));
-        System.out.println("El modulo es: " + (numero_1 % numero_2));
-        
-        System.out.println("Fin del programa");
+         /*
+         * luego vamos a mejorar el programa de operaciones el ementales
+         * pero esta ves perdiremos al usuario ingresar que operacion desea ralizar en
+         * nuestra calculadora de operaciones elementales
+         * 
+         */
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Ingrese el primer número");
+            numero_1 = entrada.nextDouble();
+            System.out.println("Ingrese el segundo número");
+            numero_2 = entrada.nextDouble();
+
+            System.out.println(numero_1 + " + " + numero_2 + " = " + (numero_1 + numero_2));
+            System.out.println(numero_1 + " - " + numero_2 + " = " + (numero_1 - numero_2));
+            System.out.println(numero_1 + " * " + numero_2 + " = " + (numero_1 * numero_2));
+
+            if (numero_2 != 0) {
+                System.out.println(numero_1 + " / " + numero_2 + " = " + (numero_1 / numero_2));
+            } else {
+                System.out.println("¡Ups! No se puede dividir por cero.");
+            }
+
+            System.out.println("¿Desea realizar más operaciones? (s/n)");
+            char respuesta = entrada.next().charAt(0);
+            if (respuesta != 's' && respuesta != 'S') {
+                continuar = false;
+            } else {
+                continuar = true;
+            }
+        }
 
     }
 }
